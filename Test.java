df5f219842f408012a554be91c21c39dc57848ad
sachin <sachinpate9@gmail.com>
2018-01-24 19:00:19 +0530

    added Hello World program

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is for the configuration of JENKINS with GIT");
+
+	}
+
+}
